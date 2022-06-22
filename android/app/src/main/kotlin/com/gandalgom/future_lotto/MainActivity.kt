4bca2d92cc9bbe2097c810a907c9a33150933078
Max Yoo <devbear74@gmail.com>
2022-06-22 17:59:06 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/gandalgom/future_lotto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gandalgom.future_lotto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Future test'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
+                ),
+                child: const Text('Future test',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text('result',
+                style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
